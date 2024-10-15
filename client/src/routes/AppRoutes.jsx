252022eb6f082bb5ae13b0c0ee0082e1a38ec071
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-10-15 18:27:52 +0600

    fix routs

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,31 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "../layout/Layout";
+import HomePage from "../pages/homePage/HomePage";
+import ItClubPage from "../pages/itCLubPage/ItClubPage";
+import Management from "../pages/management/Management";
+import { CourseListingPage } from "../pages/courseListingPage/CourseListingPage";
+import { WorkListingPage } from "../pages/workListingPage/WorkListingPage";
+import Register from "../pages/registrationPage/Register";
+
+const AppRoutes = () => {
+  return createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/itclub" element={<ItClubPage />} />
+          <Route path="/management" element={<Management />} />
+          <Route path="/course/:id" element={<CourseListingPage />} />
+          <Route path="/work/:id" element={<WorkListingPage />} />
+        </Route>
+        <Route path="/registration" element={<Register />} />
+      </>
+    )
+  );
+};
+
+export default AppRoutes;
