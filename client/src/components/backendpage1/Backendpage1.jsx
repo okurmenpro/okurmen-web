341caf4aa3816208568194a957e0deb870757e8d
aidana780 <aidanakachkynbaeva753@gmail.com>
2024-11-09 16:01:21 +0600

    Backend

--- client/src/components/backendpage1/Backendpage1.jsx
+++ client/src/components/backendpage1/Backendpage1.jsx
@@ -3,12 +3,12 @@ import Java from "../../../public/images/java.png";
 
 const Backendpage1 = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center bg-white p-6 border rounded-lg shadow-md">
+    <div className="flex flex-col md:flex-row items-center bg-white p-6  rounded-lg ">
       <div className="md:w-1/2 p-4">
-        <h2 className="text-4xl md:text-5xl font-bold text-blue-700 mb-2">
+        <h2 className="text-4xl md:text-6xl font-bold text-blue-700 mb-2">
           Backend
         </h2>
-        <p className="text-gray-700 mb-4 my-6">
+        <p className="text-gray-700 mb-4 my-6 font-medium text-xl">
           Бэкенд — это та часть любого веб-сайта или приложения, которую не
           видит пользователь, но которая обеспечивает его работу. Если
           представить сайт как ресторан, то бэкенд — это кухня, где повара
--- client/src/pages/backendPage/BackendPage.jsx
+++ client/src/pages/backendPage/BackendPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import Backendpage1 from "../../components/backendpage1/Backendpage1";
+import Backendpage2 from "../../components/backendpage2/Backendpage2";
 const BackendPage = () => {
-  return <div><Backendpage1/></div>;
+  return (
+    <div>
+      <Backendpage1 />
+      <Backendpage2 />
+    </div>
+  );
 };
 
 export default BackendPage;
