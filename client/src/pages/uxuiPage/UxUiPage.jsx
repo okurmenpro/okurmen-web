dab7bf9c7c80edf2fb63235bc019fe0e4c00dadf
saadat <devilwq99@gmail.com>
2024-11-14 16:14:26 +0600

    advantages block of UxUi page

--- client/src/pages/uxuiPage/UxUiPage.jsx
+++ client/src/pages/uxuiPage/UxUiPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../../components/header/Header";
 import UxUi1 from "../../components/uxui1/UxUi1";
 import UxUi2 from "../../components/uxui2/UxUi2";
+import CourseAdvantages from "../../components/advantagesPython/AdvantagesPython";
 
 const UxUiPage = () => {
   return (
@@ -9,6 +10,7 @@ const UxUiPage = () => {
       <Header />
       <UxUi1 />
       <UxUi2 />
+      <CourseAdvantages/>
     </>
   );
 };
