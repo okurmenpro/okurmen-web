68bc5ddb9e89aeb34f1ecc4e7a7724941972112c
tansuluu-m <karybekovatansuluu303@gmail.com>
2024-11-21 18:43:14 +0600

    Revert "main-modules python/backend"

--- client/src/pages/backendPage/BackendPage.jsx
+++ client/src/pages/backendPage/BackendPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Backendpage1 from "../../components/backendpage1/Backendpage1";
 import Backendpage2 from "../../components/backendpage2/Backendpage2";
 import ModulsBackend from "../../components/modulsBackend/ModulsBackend";
-import Benefits from "../../components/benefits/Benefits";
 
 const BackendPage = () => {
   return (
@@ -10,7 +9,6 @@ const BackendPage = () => {
       <Backendpage1 />
       <Backendpage2 />
       <ModulsBackend />
-      <Benefits/>
     </div>
   );
 };
