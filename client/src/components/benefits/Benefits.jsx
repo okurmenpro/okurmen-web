e1c7d0a60a8abd31314608279faeaeddd7f76e5d
saadat <devilwq99@gmail.com>
2024-11-21 18:09:54 +0600

    some texts

--- client/src/components/benefits/Benefits.jsx
+++ client/src/components/benefits/Benefits.jsx
@@ -5,29 +5,31 @@ const Benefits = () => {
   return (
     <div className="max-w-4xl mb-[150px] mx-auto p-6 border-2 border-orange-400 rounded-lg justify-center">
       <h2 className="text-center text-2xl font-bold mb-6">Главные модули</h2>
-
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {benefits.map((benefit, index) => {
           const borderClass = index % 3 !== 1 ? "border-r" : "";
           const borderBottomClass = index < 3 ? "border-b" : "";
 
           return (
-            <>
-            <div 
+            <div
               key={index}
-              className={`p-4 flex flex-col items-center justify-center ${borderClass} ${borderBottomClass} border-orange-300`}
+              className={`p-4 flex flex-col items-center justify-between ${borderClass} ${borderBottomClass} border-orange-300`}
             >
               <div
-                className=" mb-4"
+                className="mb-4 flex justify-center items-center flex-grow"
                 style={{ width: benefit.width, height: benefit.height }}
               >
-                <img src={benefit.img}  /> 
+                <img src={benefit.img} alt={benefit.title} />
               </div>
+              <p className="text-xl text-black-950 font-medium mt-auto">
+                {benefit.title}
+              </p>
             </div>
-            
-            </>
           );
         })}
+      </div>
+      <div className="text-2xl text-black-950 font-medium flex justify-center align-center mt-4">
+        Машинное обучение
       </div>
     </div>
   );
