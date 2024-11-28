4e879d44cafadf8d49c156c8baea37d731cfb810
saadat <devilwq99@gmail.com>
2024-11-28 15:54:53 +0600

    checboxes, clear btn, icons:trash, warning

--- client/src/components/sideBar/SideBar.jsx
+++ client/src/components/sideBar/SideBar.jsx
@@ -17,16 +17,23 @@ const SideBar = ({ isOpen, setIsOpen }) => {
   });
 
   const Checkbox = ({ selected, label, onChange }) => (
-    <label className="flex items-center gap-2 cursor-pointer ">
+    <label className="flex items-center gap-2 cursor-pointer relative">
       <input
         type="checkbox"
         checked={selected}
         onChange={onChange}
-        className="w-4 h-4 border rounded-md bg-orange-500"
+        className="w-6 h-6 appearance-none border border-gray-400 rounded-md checked:bg-orange-500 checked:border-orange-500 focus:ring-2 focus:ring-orange-500 relative"
       />
-      <span className="text-lg">{label}</span>
+      <span className="text-black">{label}</span>
+      <span
+        className={`absolute left-2 top-1/2 transform -translate-y-1/2 w-3 h-2 border-t-2 border-r-2 border-white rotate-[130deg] ${
+          selected ? "block" : "hidden"
+        }`}
+      ></span>
     </label>
   );
+  
+  
   
 
   useEffect(() => {
@@ -36,7 +43,7 @@ const SideBar = ({ isOpen, setIsOpen }) => {
       document.body.style.overflow = "auto";
     }
     return () => {
-      document.body.style.overflow = "auto"; 
+      document.body.style.overflow = "auto";
     };
   }, [isOpen]);
 
@@ -74,7 +81,6 @@ const SideBar = ({ isOpen, setIsOpen }) => {
           isOpen ? "left-0" : "-left-full"
         } w-3/4 sm:w-2/3 md:w-1/2 lg:w-[381px]`}
       >
-      
         <button
           onClick={() => setIsOpen(false)}
           className="absolute top-[120px] -right-[100px] flex items-center justify-center bg-white p-2 rounded-full shadow-md"
@@ -83,13 +89,9 @@ const SideBar = ({ isOpen, setIsOpen }) => {
         </button>
 
         <div className="pt-[30px] px-[30px] h-full overflow-y-auto flex flex-col justify-between">
-         
           <div>
-            <div className="mb-5">
-              <h2 className="text-xl font-medium">Меню</h2>
-            </div>
+            
 
-          
             <ul className="flex flex-col gap-[25px] mb-5">
               <li>
                 <button
@@ -100,24 +102,59 @@ const SideBar = ({ isOpen, setIsOpen }) => {
                   className="flex items-center font-medium text-base"
                 >
                   Заявки
-                  <img className="mt-[2px] ml-3" src="/icons/arrowRight.svg" alt="arrow" />
+                  <img
+                    className="mt-[2px] ml-3"
+                    src="/icons/arrowRight.svg"
+                    alt="arrow"
+                  />
                 </button>
               </li>
             </ul>
 
-            {/* Filters */}
             <div className="flex flex-col gap-5 text-xl font-medium">
               <p>Направление</p>
-              <Checkbox selected={checkboxState["Backend"]} label={"Backend"} onChange={() => handleCheckboxChange("Backend")} />
-              <Checkbox selected={checkboxState["UX/UI"]} label={"UX/UI"} onChange={() => handleCheckboxChange("UX/UI")} />
-              <Checkbox selected={checkboxState["Frontend"]} label={"Frontend"} onChange={() => handleCheckboxChange("Frontend")} />
+              <Checkbox
+                selected={checkboxState["Backend"]}
+                label={"Backend"}
+                onChange={() => handleCheckboxChange("Backend")}
+              />
+              <Checkbox
+                selected={checkboxState["UX/UI"]}
+                label={"UX/UI"}
+                onChange={() => handleCheckboxChange("UX/UI")}
+              />
+              <Checkbox
+                selected={checkboxState["Frontend"]}
+                label={"Frontend"}
+                onChange={() => handleCheckboxChange("Frontend")}
+              />
               <p>Статус</p>
-              <Checkbox selected={checkboxState["Гость"]} label={"Гость"} onChange={() => handleCheckboxChange("Гость")} />
-              <Checkbox selected={checkboxState["Студент"]} label={"Студент"} onChange={() => handleCheckboxChange("Студент")} />
-              <Checkbox selected={checkboxState["Выпускник"]} label={"Выпускник"} onChange={() => handleCheckboxChange("Выпускник")} />
+              <Checkbox
+                selected={checkboxState["Гость"]}
+                label={"Гость"}
+                onChange={() => handleCheckboxChange("Гость")}
+              />
+              <Checkbox
+                selected={checkboxState["Студент"]}
+                label={"Студент"}
+                onChange={() => handleCheckboxChange("Студент")}
+              />
+              <Checkbox
+                selected={checkboxState["Выпускник"]}
+                label={"Выпускник"}
+                onChange={() => handleCheckboxChange("Выпускник")}
+              />
               <p>Уровень</p>
-              <Checkbox selected={checkboxState["Internship"]} label={"Internship"} onChange={() => handleCheckboxChange("Internship")} />
-              <Checkbox selected={checkboxState["Bootcamp"]} label={"Bootcamp"} onChange={() => handleCheckboxChange("Bootcamp")} />
+              <Checkbox
+                selected={checkboxState["Internship"]}
+                label={"Internship"}
+                onChange={() => handleCheckboxChange("Internship")}
+              />
+              <Checkbox
+                selected={checkboxState["Bootcamp"]}
+                label={"Bootcamp"}
+                onChange={() => handleCheckboxChange("Bootcamp")}
+              />
             </div>
           </div>
 
