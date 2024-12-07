3cd999829e355cbb07003b0628eb3f644a4428c3
saadat <devilwq99@gmail.com>
2024-12-07 16:15:48 +0600

    header speed

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,82 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Navbar from "../navbar/Navbar";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../../ui/buttons/Button";
+
 const navlinks = [
-  {
-    title: "Курстар",
-    link: "#course",
-  },
-  {
-    title: "Компания жөнүндө",
-    link: "#company-info",
-  },
-  {
-    title: "Менторлор",
-    link: "#mentors",
-  },
+  { title: "Курстар", link: "#course" },
+  { title: "Компания жөнүндө", link: "#company-info" },
+  { title: "Менторлор", link: "#mentors" },
 ];
 
 const Header = () => {
   const [open, setOpen] = useState(false);
-  const [isScroll, setIsScroll] = useState(false);
-
-  useEffect(() => {
-    function scrollBar() {
-      if (window.scrollY >= 0) {
-        setIsScroll(true);
-      } else {
-        setIsScroll(false);
-      }
-    }
-
-    window.addEventListener("scroll", scrollBar);
-  }, []);
 
   const handleMenu = () => {
     setOpen((prev) => !prev);
   };
 
   return (
-    <header
-      className={`'sm:px-6 w-full bg-white ${
-        isScroll ? "is-scroll" : "no-scroll"
-      }`}
-    >
-      <div className="container">
-        <div className="flex items-center justify-between ">
-          <div className="ml-3 md:ml-0 mx-[30px]">
-            <Link to={'/'}>
+    <header className="fixed top-0 left-0 w-full bg-white z-50 shadow-lg">
+      <div className="container mx-auto flex items-center justify-between py-4">
+        <div className="ml-3 md:ml-0 mx-[30px]">
+          <Link to={"/"}>
             <img
-              className="mx-[40px] cursor-pointer"
+              className="cursor-pointer"
               width={80}
-              src="/public/images/logo.png"
-              alt=""
+              src="/images/logo.png"
+              alt="Logo"
             />
-            </Link>
-          </div>
-          <Navbar navlinks={navlinks} />
-          <div>
-            <Link to={"/registration"}>
-              <button className="text-[15px] bg-black text-white p-[10px] rounded-[5px]  	">
-                Зарегистрироваться
-              </button>
-            </Link>
-          </div>
-          <div className="hidden md:block">
-            <Button text="Консультация" />
-          </div>
-          <div className=" flex md:hidden">
-            <button onClick={handleMenu} className="w-7">
-              {open === true ? <FaTimes /> : <FaBars />}
+          </Link>
+        </div>
+        <Navbar navlinks={navlinks} />
+        <div>
+          <Link to={"/registration"}>
+            <button className="text-[15px] bg-black text-white p-[10px] rounded-[5px]">
+              Зарегистрироваться
             </button>
-          </div>
+          </Link>
+        </div>
+        <div className="hidden md:block">
+          <Button text="Консультация" />
+        </div>
+        <div className="flex md:hidden">
+          <button onClick={handleMenu} className="w-7">
+            {open ? <FaTimes /> : <FaBars />}
+          </button>
         </div>
       </div>
-      {open ? (
-        <div className="md:hidden absolute bg-[#00000068] h-screen w-full mt-4 ">
-          <div className="absolute w-full z-20	 bg-white rounded-b-3xl pb-3 space-y-1 sm:px-3 md:px-0 shadow-lg transition-all ease-in-out duration-500">
+      {open && (
+        <div className="md:hidden absolute bg-[#00000068] h-screen w-full mt-4">
+          <div className="absolute w-full z-20 bg-white rounded-b-3xl pb-3 space-y-1 sm:px-3 md:px-0 shadow-lg">
             {navlinks.map((link, idx) => (
               <a
                 className="text-black hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
@@ -88,7 +61,7 @@ const Header = () => {
             ))}
           </div>
         </div>
-      ) : null}
+      )}
     </header>
   );
 };
