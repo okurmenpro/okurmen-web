faf411185910ed7c03f16f469681059022a90806
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-12-09 21:50:58 +0600

    scroll header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../navbar/Navbar";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
-
 
 const navlinks = [
   { title: "Курстар", link: "#course" },
@@ -13,38 +12,48 @@ const navlinks = [
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [isScroll, setIsScroll] = useState(false);
+
+  useEffect(() => {
+    function scrollBar() {
+      if (window.scrollY >= 50) {
+        setIsScroll(true);
+      } else {
+        setIsScroll(false);
+      }
+    }
+    window.addEventListener("scroll", scrollBar);
+  }, []);
 
   const handleMenu = () => {
     setOpen((prev) => !prev);
   };
 
   return (
-    <header className="fixed left-0 w-full bg-white z-50 shadow-lg top-0">
-      <div className="container mx-auto flex items-center justify-between py-6">
+    <header
+      className={`fixed left-0 w-full bg-white z-50  top-0 ${
+        isScroll ? "py-2 duration-150 shadow-lg" : "py-5 duration-150"
+      }`}
+    >
+      <div className="container mx-auto flex items-center justify-between py-4">
         <div className="ml-3 md:ml-0 mx-[30px]">
           <Link to={"/"}>
             <img
-
               className="cursor-pointer"
               width={80}
-
               src="/images/logo.png"
               alt="Logo"
             />
-
           </Link>
         </div>
         <Navbar navlinks={navlinks} />
-        <div className='flex gap-4 items-center'>
-        <Button
-              ButtonText="Войти"
-              color="border_black"
-            />
-              <Button
-                ButtonText="Зарегестрироваться"
-                color="black"
-                to="/registration"
-              />
+        <div className="flex gap-4 items-center">
+          <Button ButtonText="Войти" color="border_black" />
+          <Button
+            ButtonText="Зарегестрироваться"
+            color="black"
+            to="/registration"
+          />
         </div>
         <div className="flex md:hidden">
           <button onClick={handleMenu} className="w-7">
@@ -59,7 +68,6 @@ const Header = () => {
             {navlinks.map((link, idx) => (
               <a
                 className="text-black hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-
                 key={idx}
                 href={link.link}
               >
@@ -68,12 +76,9 @@ const Header = () => {
             ))}
           </div>
         </div>
-      )
-
-      }
+      )}
     </header>
   );
 };
 
-
-export default Header
+export default Header;
