96ec0a1d832bdf6e4a0a5b519fe80aa498eb7230
MahmudovIbragim <ibrahimorunbaev59@gmail.com>
2024-12-12 19:19:04 +0600

    resposive header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import Navbar from "../navbar/Navbar";
-import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Button from "../button/Button";
 
 const navlinks = [
-  { title: "О компании", link: "#company-info" },
-  { title: "Курсы", link: "#course" },
-  { title: "Менторы", link: "#mentors" },
+  { id: 1, title: "О компании", link: "#company-info" },
+  { id: 2, title: "Курсы", link: "#course" },
+  { id: 3, title: "Менторы", link: "#mentors" },
+  { id: 4, title: "Стажировка", link: "#internship" },
+  { id: 5, title: "It-club", link: "#it-club" },
 ];
 
 const Header = () => {
@@ -36,19 +37,18 @@ const Header = () => {
       }`}
     >
       <div className="container mx-auto flex items-center justify-between py-4">
-        <div className="ml-3 md:ml-0 mx-[30px]">
+        <div className="ml-3 md:ml-0 ">
           <Link to={"/"}>
             <img
-              className="cursor-pointer"
+              className="cursor-pointer max-w-[70px] w-full"
               width={80}
               src="/images/logo.png"
               alt="Logo"
             />
           </Link>
         </div>
-        <Navbar  />
-        <div className="flex gap-4 items-center">
-          <Button ButtonText="Войти" color="border_black" />
+        <Navbar navlinks={navlinks} />
+        <div className="flex w-fit gap-4 items-center">
           <Button
             ButtonText="Зарегестрироваться"
             color="black"
@@ -56,14 +56,26 @@ const Header = () => {
           />
         </div>
         <div className="flex md:hidden">
-          <button onClick={handleMenu} className="w-7">
-            {open ? <FaTimes /> : <FaBars />}
+          <button className="w-7">
+            <label>
+              <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
+                <input
+                  checked={open}
+                  onChange={handleMenu}
+                  className="hidden peer"
+                  type="checkbox"
+                />
+                <div className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"></div>
+                <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"></div>
+                <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"></div>
+              </div>
+            </label>
           </button>
         </div>
       </div>
 
       {open && (
-        <div className="md:hidden absolute bg-[#00000068] h-screen w-full mt-4">
+        <div className="md:hidden absolute  bg-[#00000068] h-screen w-full ">
           <div className="absolute w-full z-20 bg-white rounded-b-3xl pb-3 space-y-1 sm:px-3 md:px-0 shadow-lg">
             {navlinks.map((link, idx) => (
               <a
