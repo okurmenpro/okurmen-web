3a5b032219aa25d105af4110d0d483680279f04a
Saadat <devilwq99@gmail.com>
2025-02-18 14:27:31 +0600

    fix:hero section:btn, header:drowdowns

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,21 +2,8 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { courses } from "../../data/courses";
 
-const Navbar = ({
-  navlinks,
-  clicked,
-  hovered,
-  handleMobileClick,
-  handleMouseEnter,
-  handleMouseLeave,
-}) => {
-  const handleEnter = () => {
-    handleMouseEnter();
-  };
-
-  const handleLeave = () => {
-    handleMouseLeave();
-  };
+const Navbar = ({ navlinks }) => {
+  const [hovered, setHovered] = useState(false);
 
   return (
     <nav className="hidden md:flex w-full justify-center">
@@ -26,8 +13,8 @@ const Navbar = ({
             <li
               key={item.id}
               className="relative"
-              onMouseEnter={handleEnter}
-              onMouseLeave={handleLeave}
+              onMouseEnter={() => item.id === 2 && setHovered(true)}
+              onMouseLeave={() => item.id === 2 && setHovered(false)}
             >
               <Link
                 to={item.link}
@@ -37,36 +24,19 @@ const Navbar = ({
               </Link>
 
               {item.id === 2 && hovered && (
-                <div className="absolute top-full left-0 w-max bg-white shadow-lg rounded-md">
+                <ul className="absolute top-full left-0 w-max bg-white shadow-lg rounded-md ">
                   {Array.isArray(courses) &&
                     courses.map((course) => (
                       <li key={course.name}>
                         <Link
                           to={course.path}
-                          className="block text-left pl-[10px] py-2 pr-12 text-black hover:text-gray-600 transition-colors"
+                          className="block px-4 py-2 text-black hover:text-gray-600 transition-colors"
                         >
                           {course.name}
                         </Link>
                       </li>
                     ))}
-                </div>
-              )}
-
-              {item.id === 2 && clicked === item.id && (
-                <div className="absolute top-full left-0 w-max bg-white shadow-lg mt-2 rounded-md">
-                  {Array.isArray(courses) &&
-                    courses.map((course) => (
-                      <li key={course.name}>
-                        <Link
-                          to={course.path}
-                          className="block py-2 px-4 text-black hover:text-gray-600 transition-colors"
-                          onClick={() => setClicked(null)}
-                        >
-                          {course.name}
-                        </Link>
-                      </li>
-                    ))}
-                </div>
+                </ul>
               )}
             </li>
           ))}
