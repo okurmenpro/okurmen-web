f5e2fd87a484b3891e77f34e76b89dc20948fe79
kulpunai180 <jekshenbekova@outlook.com>
2025-02-18 16:00:19 +0600

    module for java

---
+++ client/src/data/javaModules.js
@@ -0,0 +1,36 @@
+import springBoolt from '../../public/icons/springBoolt.svg'
+import hibernate from '../../public/icons/hibernate.svg'
+import springSecurity from '../../public/icons/springSecurety.svg'
+import docker from '../../public/icons/docker.svg'
+import apacheKafka from '../../public/icons/apacheKafka.svg'
+import postgre from '../../public/icons/postgre.svg'
+
+
+const JavaModules = [
+    {
+        img: springBoolt,
+        title: 'Spring Boolt'
+    },
+    {
+        img: hibernate,
+        title: 'Hibernate'
+    },
+    {
+        img: springSecurity,
+        title: 'Spring Security'
+    },
+    {
+        img: docker,
+        title: 'Docker'
+    },
+    {
+        img: apacheKafka,
+        title: 'Apache Kafka'
+    },
+    {
+        img: postgre,
+        title: 'PostgreSQL'
+    }
+]
+
+export default JavaModules
